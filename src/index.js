91df506c13f5d873e4308da6e3fa9c724b452ec7
luongbs01 <luongbs01@gmail.com>
2022-04-02 17:16:23 +0700

    static files

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(morgan('combined'))
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, './resources/views'));
 
