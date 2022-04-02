b703e498b3591c991ca88c412eba0166dff7bcef
luongbs01 <luongbs01@gmail.com>
2022-04-02 17:00:40 +0700

    handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
+const path = require('path')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, './resources/views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
 })
 
 app.listen(port, () => {
