39875987737feb03d51b2f2a3548efdac8a2fa54
luongbs01 <luongbs01@gmail.com>
2022-04-03 12:00:08 +0700

    Prettier
    1. npm i prettier lint-staged husky --save-dev
    2. update scripts in package.json
    3. npm run beautiful

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const { engine } = require('express-handlebars')
-const path = require('path')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
 
 // app.use(morgan('combined'))
 
@@ -20,8 +22,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, './resources/views'));
 
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
