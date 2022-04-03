a990a2b93123724ba9bb259c4027a6c50266555d
luongbs01 <luongbs01@gmail.com>
2022-04-03 11:14:02 +0700

    [MVC] Routes & Controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({
@@ -18,23 +20,7 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, './resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  console.log(req.query)
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('');
-})
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
