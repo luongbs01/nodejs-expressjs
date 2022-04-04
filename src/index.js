5f63f43c24497f87351e8412ce7dbd5272341b35
luongbs01 <luongbs01@gmail.com>
2022-04-04 11:13:00 +0700

    [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
@@ -20,9 +21,19 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // app.use(morgan('combined'))
 
-app.engine('.hbs', engine({ extname: '.hbs' }));
+app.engine(
+  '.hbs',
+  engine({
+    extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
