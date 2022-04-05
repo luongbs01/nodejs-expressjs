7945fbf51f7a7ecb55e8d6ed57dfb3f4ab8c66ef
luongbs01 <luongbs01@gmail.com>
2022-04-05 12:05:55 +0700

    Fix bug

--- src/app/model/Course.js
+++ src/app/model/Course.js
@@ -11,8 +11,6 @@ const Course = new Schema(
     videoId: { type: String, maxlength: 600 },
     level: { type: String, maxlength: 600 },
     image: { type: String, maxlength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
     slug: { type: String, slug: 'name', unique: true },
   },
   {
