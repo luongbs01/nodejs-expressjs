f1e1c87c69cb1a65d38e3a068579f7063eb6d53e
luongbs01 <luongbs01@gmail.com>
2022-04-05 11:46:58 +0700

    Sort middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -23,6 +25,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Custom middlewares
+app.use(sortMiddleware);
+
 // app.use(morgan('combined'))
 
 app.engine(
@@ -31,6 +36,26 @@ app.engine(
     extname: '.hbs',
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, _sort) => {
+        const sortType = field === _sort.column ? _sort.type : 'default';
+        const icons = {
+          default: 'oi oi-elevator',
+          asc: 'oi oi-sort-ascending',
+          desc: 'oi oi-sort-descending',
+        };
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc',
+        };
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+          <span class="${icon}"></span>
+        </a>`;
+      },
     },
   }),
 );
